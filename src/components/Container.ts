bfdeecbc999d6cf8cddec51d9bba1f15ea3ec191
Adry <adrydevmateo@gmail.com>
2022-11-27 14:41:09 -0400

    Finished working on PasswordSettings component

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -1,3 +1,10 @@
 const containerElement: HTMLElement = document.createElement("section");
+const passIndicator: HTMLElement = document.createElement("section");
+const buttonGenerate: HTMLButtonElement = document.createElement("button");
+
+passIndicator.setAttribute("class", "pass-indicator");
+buttonGenerate.setAttribute("class", "generate-btn");
+
+buttonGenerate.innerText = "Generate Password";
 
 export const Container: HTMLElement = containerElement;
